c9f64a695c30790095bb085292b3d4ba1a5b3d4f
HenyPatel01 <104871631+HenyPatel01@users.noreply.github.com>
2023-11-06 23:13:14 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaQIdkZDkd_7G3kkvpt9wYGWloDXzQmIM",
+  authDomain: "spectagram-app-9a0e8.firebaseapp.com",
+  projectId: "spectagram-app-9a0e8",
+  storageBucket: "spectagram-app-9a0e8.appspot.com",
+  messagingSenderId: "299132851294",
+  appId: "1:299132851294:web:90e0f9b8f1fdadeb4dd0cf"
+};
